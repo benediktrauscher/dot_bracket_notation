8f9f07a1b38327b011818f88e5760c3ca94ff563
bene200 <berau90@gmail.com>
2015-02-18 16:52:01 +0100

    monotype characters for textox, bigger textboxes and different textbox positioning, . insertion when character is added

--- src/parseinput.js
+++ src/parseinput.js
@@ -1,6 +1,7 @@
 function getInputSequences() {
-  var seqBox = document.getElementById('SEQ_BOX');
-  var dotbrBox = document.getElementById('DOTBR_BOX');
+  "use strict";
+  var seqBox = document.getElementById("SEQ_BOX");
+  var dotbrBox = document.getElementById("DOTBR_BOX");
   var sequence = seqBox.value;
   var dotbr = dotbrBox.value;
 
@@ -8,51 +9,76 @@ function getInputSequences() {
 }
 
 function main() {
+  "use strict";
   //initalize input boxes with example structure
-  document.getElementById('SEQ_BOX').value = "CAGCACGACACUAGCAGUCAGUGUCAGACUGCARACAGCACGACACUAGCAGUCAGUGUCAGACUGCARACAGCACGACACUAGCAGUCAGUGUCAGACUGCARA";
-  document.getElementById('DOTBR_BOX').value = "..(((((...(((((...(((((...(((((.....)))))...))))).....(((((...(((((.....)))))...))))).....)))))...)))))..";
+  document.getElementById("SEQ_BOX").value = "CAGCACGACACUAGCAGUCAGUGUCAGACUGCARACAGCACGACACUAGCAGUCAGUGUCAGACUGCARACAGCACGACACUAGCAGUCAGUGUCAGACUGCARA";
+  document.getElementById("DOTBR_BOX").value = "..(((((...(((((...(((((...(((((.....)))))...))))).....(((((...(((((.....)))))...))))).....)))))...)))))..";
   //init colors
-  $("#acolor").spectrum({ color: "#64F73F" });
-  $("#ccolor").spectrum({ color: "#FFB340" });
-  $("#gcolor").spectrum({ color: "#EB413C" });
-  $("#ucolor").spectrum({ color: "#3C88EE" });
+  $("#acolor").spectrum({color: "#64F73F"});
+  $("#ccolor").spectrum({color: "#FFB340"});
+  $("#gcolor").spectrum({color: "#EB413C"});
+  $("#ucolor").spectrum({color: "#3C88EE"});
   //init alert box
-  document.getElementById('ALERT').value = "";
+  document.getElementById( "ALERT" ).value = "";
 
   var input = getInputSequences();
   var struct = transformDotBracket(input[0], input[1]);
   visCytoscapeJs(struct);
 
-  var runButton = document.getElementById('PERFORM_VIS');
+  var runButton = document.getElementById("PERFORM_VIS");
   runButton.readOnly = true;
-  runButton.addEventListener('click', function(){ 
-    document.getElementById('ALERT').value = "";
+  runButton.addEventListener("click", function(){ 
+    document.getElementById("ALERT").value = "";
   	var input = getInputSequences();
   	if(checkConditions(input)){
   		var struct = transformDotBracket(input[0], input[1]);
   		visCytoscapeJs(struct);
   	}
   }, false);
+
+  var seqBox = document.getElementById("SEQ_BOX");
+  seqBox.addEventListener("keypress", function(){
+      var pos = doGetCaretPosition(seqBox);
+      var dotbrBox = document.getElementById("DOTBR_BOX");
+      dotbrBox.value = [dotbrBox.value.slice(0, pos), ".", dotbrBox.value.slice(pos)].join("");
+  }, false);
 }
+
 $( document ).ready(main);
 
 function checkConditions(sequences){
+  "use strict";
   var isFine = true;
   if(sequences[0].length === 0 || sequences[1].length === 0){
     isFine = false;
-    document.getElementById('ALERT').value = "Please enter a sequence!";
+    document.getElementById("ALERT").value = "Please enter a sequence!";
   }
   else if(sequences[0].length != sequences[1].length){
     isFine = false;
-    document.getElementById('ALERT').value = "Sequences must have equal length!";
+    document.getElementById("ALERT").value = "Sequences must have equal length!";
   }
-  else if(! sequences[1].match('^[().]+$')){
+  else if(! sequences[1].match("^[().]+$")){
     isFine = false;
-    document.getElementById('ALERT').value = "Dot-bracket sequence may only contain \"(\", \")\", or \".\"!";
+    document.getElementById("ALERT").value = "Dot-bracket sequence may only contain \"(\", \")\", or \".\"!";
   }
-  else if(! sequences[0].match('^[ACGUTRYSWKMBDHVN\-]+$')){
+  else if(! sequences[0].match("^[ACGUTRYSWKMBDHVN-]+$")){
     isFine = false;
-    document.getElementById('ALERT').value = "Sequence may only contain IUPAC-characters!";
+    document.getElementById("ALERT").value = "Sequence may only contain IUPAC-characters!";
   }
   return isFine;
+}
+
+function doGetCaretPosition (ctrl) {
+  var CaretPos = 0;  
+  // IE Support
+  if (document.selection) {
+    ctrl.focus ();
+    var Sel = document.selection.createRange ();
+    Sel.moveStart ('character', -ctrl.value.length);
+    CaretPos = Sel.text.length;
+  }
+  // Firefox support
+  else if (ctrl.selectionStart || ctrl.selectionStart == '0')
+    CaretPos = ctrl.selectionStart;
+  return (CaretPos);
 }
--- src/visualizerna.js
+++ src/visualizerna.js
@@ -6,26 +6,26 @@ function visCytoscapeJs(graph) {
 	var cyEle = toCytoscapeElements(graph);
 
 	var cy = cytoscape({
-  		container: document.getElementById('cy'),
+  		container: document.getElementById("cy"),
   
   		style: cytoscape.stylesheet()
-	    		.selector('node')
+	    		.selector("node")
 	      		.css({
-			        'content': 'data(label)',
-			        'text-valign': 'center',
-			        'color': 'white',
-			        'text-outline-width': 2,
-			        'text-outline-color': '#778899'
+			        "content": "data(label)",
+			        "text-valign": "center",
+			        "color": "white",
+			        "text-outline-width": 2,
+			        "text-outline-color": "#778899"
 	      		})
-	    		.selector(':selected')
+	    		.selector(":selected")
 	      		.css({
-			        'background-color': 'black',
-			        'line-color': 'black',
-			        'target-arrow-color': 'black',
-			        'source-arrow-color': 'black',
-			        'text-outline-color': 'black'
+			        "background-color": "black",
+			        "line-color": "black",
+			        "target-arrow-color": "black",
+			        "source-arrow-color": "black",
+			        "text-outline-color": "black"
 	      		})
-	      		.selector('edge')
+	      		.selector("edge")
 	      		.css({
 	      			
 	      		}),
@@ -35,7 +35,7 @@ function visCytoscapeJs(graph) {
   		layout: {
   			//Use preset layout with precalculated 
   			//nucleotide coordinates
-    		name: 'preset',
+    		name: "preset",
   		},			
       
       	ready: function(){
@@ -44,36 +44,36 @@ function visCytoscapeJs(graph) {
  				pan: { x: 100, y: 100 }
 			});
 			cy.fit();
-			document.getElementById('cy').childNodes[0]
+			document.getElementById("cy").childNodes[0]
 				.childNodes[4].style.position = "relative";
-      		document.getElementById('cy').childNodes[0]
+      		document.getElementById("cy").childNodes[0]
       			.style.position = "relative";
-      		document.getElementById('cy').style.position = "absolute";
+      		document.getElementById("cy").style.position = "absolute";
 
-      		var centerButton = document.getElementById('CENTER');
+      		var centerButton = document.getElementById("CENTER");
   			centerButton.readOnly = true;
-  			centerButton.addEventListener('click', function(){ 
+  			centerButton.addEventListener("click", function(){ 
   				cy.center();
   				cy.fit();
   			}, false);
 
-  			var exportButton = document.getElementById('EXPORT');
+  			var exportButton = document.getElementById("EXPORT");
   			exportButton.readOnly = true;
-  			exportButton.addEventListener('click', function(){
+  			exportButton.addEventListener("click", function(){
     			var png64 = cy.png();
-    			newTab = window.open();
-    			newTab.document.write('<img src="'+png64+'"/>');
+    			var newTab = window.open();
+    			newTab.document.write( "<img src=" + png64 + "/>" );
     			newTab.focus();
   			}, false);
       	}
 	})
 	
 	//Display nucleotide index on mouseover
-	cy.on('mouseover', 'node', function(event){
+	cy.on("mouseover", "node", function(event){
 		var nd = event.cyTarget;
 		Tip(parseInt(nd.id(), 10)+1);
 	})
-	cy.on('mouseout', 'node', function(event){
+	cy.on("mouseout", "node", function(event){
 		var nd = event.cyTarget;
 		UnTip();
 	})
@@ -86,7 +86,7 @@ function visCytoscapeJs(graph) {
 		}
 	});
 	
-	cy.on('tap', 'node', function(event){
+	cy.on("tap", "node", function(event){
 		if(drawEdge && $.isEmptyObject(srcNode)){
 			srcNode = event.cyTarget;
 			console.log("new source node specified");
@@ -103,7 +103,7 @@ function visCytoscapeJs(graph) {
 			targetNode = {};
 
 			inputStr = graphToStrings(graph);
-			document.getElementById('DOTBR_BOX').value = inputStr.dotbr;
+			document.getElementById("DOTBR_BOX").value = inputStr.dotbr;
 			visCytoscapeJs(transformDotBracket(inputStr.seq, inputStr.dotbr));
 
 		} else {
